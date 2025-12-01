d5d8957c44dd4bbdfe80aae58679493388de7712
elizazha <lizokbella@mail.ru>
2025-12-01 17:49:09 +0300

    added everything important(not ready)

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 namespace top{
+
   struct p_t{
     int x, y;
   };
@@ -8,6 +9,7 @@ namespace top{
     virtual p_t begin() const = 0;
     virtual p_t next(p_t) const = 0;
   };
+
   bool operator == (p_t a, p_t b)
   {
     return a.x == b.x && a.y != b.y; // оператор туда-сюда(space shift(?))
@@ -23,14 +25,48 @@ namespace top{
     p_t o;
     Dot(int x, int y);
   };
-}
 
+  struct frame_t{
+    p_t left_bot;
+    p_t right_bot;
+  };
+
+  void make_f(IDraw **b, size_t k);
+  void get_points(IDraw *b, p_t **ps, size_t &s);
+  frame_t build_frame(const p_t *ps, size_t s);
+  char *build_canvas(frame_t f);
+  void paint_canvas(char *cnv, frame_t fr, const p_t *ps, size_t k, char f);//берем канву,рамку, все точки и кол-во точек и символ, которым заполняем
+  void print_canvas (const char *cnv, frame_t fr);
+}
 
 int main()
 {
   using namespace top;
+  IDraw *f[3] = {};
+  p_t *p = nullptr;
+  char *cnv = nullptr;
+  size_t s = 0;
+  int err = 0;
+  try{
+    make_f(f, 3);
+    for (size_t i = 0; i < 3; ++i)
+    {
+      get_points(f[i], &p, s);
+    }
+    frame_t fr = build_frame(p, s);
+    cnv = build_canvas(fr);
+    paint_canvas(cnv, fr, p, s, '#');
+    print_canvas(cnv, fr);
+  }
+  catch(...)
+  {
+    err = 1;
+  }
+  delete f[0]; delete f[1]; delete f[2];
+  delete[] p;
+  delete[] cnv;
+  return err;
 }
-
 top::Dot::Dot(int x, int y):
   IDraw(),
   o{x, y}
