25e8ea934c92776e36e87ba57dcf10c7b8bcfd4a
elizazha <lizokbella@mail.ru>
2025-12-01 16:59:19 +0300

    IDraw interface

--- main.cpp
+++ main.cpp
@@ -1,4 +1,23 @@
 #include <iostream>
+namespace top{
+  struct p_t{
+    int x, y;
+  };
+
+  struct IDra{
+    virtual p_t begin() = 0;
+    virtual p_t next(p_t) = 0;
+  };
+  bool operator == (p_t a, p_t b)
+  {
+    return a.x == b.x && a.y != b.y; // оператор туда-сюда(space shift(?))
+  }
+  bool operator == (p_t a, p_t b)
+  {
+    return !(a==b);
+  }
+}
 int main()
 {
+  using namespace top;
 }
